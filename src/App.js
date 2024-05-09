53caf57b955ab87599f6128f13bc054ade36537d
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-05-09 09:38:33 +0545

    chnages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+
+
+  const handleAlert = () => {
+    alert('don\'t copy');
+  }
+
+  const eventCall = (e, msg) => {
+    console.log(e);
+    alert(msg);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-4'>
+      <button onClick={() => eventCall('hello jee')} className='bg-slate-600 text-white p-2 rounded' >Click Me</button>
+
+      <p > <span onCopy={handleAlert}>Lorem </span> ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sunt voluptatum alias molestiae, repellendus ad? Alias culpa recusandae architecto natus.</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/dummy.js
@@ -0,0 +1,10 @@
+
+
+
+export const age = 90;
+
+export const greet = () => {
+  console.log('hello world');
+}
+
+
