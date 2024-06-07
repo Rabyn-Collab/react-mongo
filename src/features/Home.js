5565f37290c55c64586a17dca4deec3bb1fe068a
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-06-07 08:46:52 +0545

    changes

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,26 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router';
 
 const Home = () => {
-  const { users } = useSelector((state) => state.userSlice);
-
-
   return (
-    <div>
-
-      <div onClick={() => {
-        document.getElementsByClassName('.root').innerHTML = 'red';
-        console.log('hello');
-      }} className="root">
-        <h1>as;ldjsa;dlj;l</h1>
-      </div>
-
-
-
-
-
-    </div>
+    <div>Home</div>
   )
 }
 
